db715dab1a068b7af16c62636a63d1edead2f82d
Nirali Thakkar <87859904+Nirali-Thakkar@users.noreply.github.com>
2024-05-13 09:13:55 +0000

    done

---
+++ 01_basic/03_operation.js
@@ -0,0 +1,14 @@
+let a = 45;
+let b = 4;
+console.log(a+b)
+console.log(a/b)
+console.log(a**b) // a to the power b
+
+console.log("1" + 2) //will give 12
+console.log(1 + "2") //will give 12
+console.log("1" + 2 + 2) // will give 122
+console.log(1 + 2 + "3") // will give 33
+console.log(3 + 4 * 5 % 3)
+
+console.log(+true) //conversion - will give 1
+console.log(+"") //covcersion - will give 0
