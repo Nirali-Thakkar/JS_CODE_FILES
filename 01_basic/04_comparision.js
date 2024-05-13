d9f7a19f829c2c20429630f02b8a07609628a0e5
Nirali Thakkar <87859904+Nirali-Thakkar@users.noreply.github.com>
2024-05-13 17:49:30 +0000

    done

---
+++ 01_basic/04_comparision.js
@@ -0,0 +1,9 @@
+console.log("2" > 1) //true as it 2 is converted to number
+console.log("02" > 3)  // false
+
+console.log(null > 0) //false
+console.log(null == 0) //false
+console.log(null >= 0) //true as 0 ==0 
+//the reason is copmapraion >= <= > < work defferently than equality check ==
+//comparison convert null to  number to 0
+//=== also chcek data type with valur
