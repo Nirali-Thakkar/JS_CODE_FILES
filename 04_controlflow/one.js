d705ee283939a0accebeda5480a7a26235319642
Nirali Thakkar <87859904+Nirali-Thakkar@users.noreply.github.com>
2024-05-16 17:51:44 +0000

    done

---
+++ 04_controlflow/one.js
@@ -0,0 +1,28 @@
+
+
+// 2 =="2" will be true 
+// 2 ==="2" will not be true 
+
+let t = 41 
+if( t< 50){
+    console.log("t less than 50 ")
+}
+console.log("t greater than 50 ")
+
+//switch 
+
+let month = 4
+switch(month){
+    case 1 :
+        console.log("one")
+        break;
+    case 2 :
+        console.log("two")
+        break;
+    case 3 : 
+        console.log("Three")
+        break;
+    default :
+        console.log("default")
+
+}
