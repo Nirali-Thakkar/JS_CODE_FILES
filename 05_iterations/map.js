c185fdae469bb4390bf63f6776b8a9f37e1f53f3
Nirali Thakkar <87859904+Nirali-Thakkar@users.noreply.github.com>
2024-05-17 18:12:56 +0000

    done

---
+++ 05_iterations/map.js
@@ -0,0 +1,11 @@
+let mynum = [1,2,3,4,5,6,7,8,9,10]
+//map returns new array 
+const n = mynum.map( (num) => num+10)
+console.log(n)
+
+//when you add scope {} you need to return 
+
+
+//chaining 
+const m = mynum.map( (num) => num*10 ).map( (num) => num+1 ).filter((num) => num>40)
+console.log(m)
