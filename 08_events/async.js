e73727fd683af6e30ff5a2887a3c38efeb70339c
Nirali Thakkar <87859904+Nirali-Thakkar@users.noreply.github.com>
2024-05-19 13:26:38 +0000

    done

---
+++ 08_events/async.js
@@ -0,0 +1,34 @@
+//default javascript is 
+    //syncronus - code execution one by one 
+    //single thread -all execution in none thread 
+
+//execution 
+    //each opeartion wiats for the last one to complete 
+//blocking code and non blocking code 
+        // blocking code
+            //block the execution of code
+            //read file sync
+        // non blocking code
+            //does not block execution of code 
+            //read file async
+
+//web api 
+    //DOM
+    //set time out
+    //set interval
+
+//task queue
+    //add task to call stack
+
+//example
+    //print 1
+    //timeinterval(0) print 2
+    //print 3
+
+    //op
+    1 
+    3
+    2
+    //because timeinterval is added to call stack
+
+//fetch api have high priority comes from priority queue
